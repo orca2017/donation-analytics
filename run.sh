9f3435d5233054e14862b0bebd4cf521fd7df908
Xinzhan Bai <33246438+orca2017@users.noreply.github.com>
2018-02-13 04:20:30 -0500

    Update run.sh
    add echo information

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
 # compile program
+echo compiling program...
 g++ -std=c++11 -O2 -g -o main ./src/main.cpp ./src/Utils.cpp
 #
 # run program
+echo running program...
 #./main itcont.txt percentile.txt output.txt
 ./main ./input/itcont.txt ./input/percentile.txt ./output/repeat_donors.txt
